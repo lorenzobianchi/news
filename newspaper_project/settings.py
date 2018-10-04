d4f74ff69219637dfa879d4361bb085d237c6112
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2018-10-04 09:58:52 +0200

    deleted sendgrid info

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -130,8 +130,3 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'lorebianchi123'
-EMAIL_HOST_PASSWORD = 'django2016'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
